b0544ed9a9c6e7b96568b4ed32841d51869e8304
Jason Ballejo <ballejo19@gmail.com>
2023-10-09 18:15:05 -0400

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={classNames(inter.className, "h-screen")}>{children}</body>
     </html>
   )
 }
