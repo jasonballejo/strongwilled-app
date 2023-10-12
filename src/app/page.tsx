1abc525eb33e6911c33e2c3ac8546d9be87d4fec
Jason Ballejo <ballejo19@gmail.com>
2023-10-12 19:07:25 -0400

    fix to useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
 import ClientsTable from "@/components/ClientsTable";
 import TopSection from "@/components/TopSection";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
-const preline = require("preline");
 
 export default function Home() {
-  useEffect(() => {
-    import(preline);
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -1,11 +1,11 @@
-import ButtonStyle from "./Button/ButtonStyle";
+import ButtonModal from "./Button/ButtonModal";
 
 export default function TopSection() {
   return (
-    <div className="flex justify-between items-center px-5">
-      <div className="">Clients</div>
+    <div className="flex justify-between items-center px-5 py-5">
+      <div className="text-2xl font-bold uppercase">Clients List</div>
       <div className="">
-        <ButtonStyle title="+ Client" />
+        <ButtonModal title="+ Client" />
       </div>
     </div>
   );
